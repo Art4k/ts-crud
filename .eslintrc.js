07de776d09c505e85a7cd908b8ca1c87fb9456e8
Artūras <arturas008@gmail.com>
2023-01-18 19:07:31 +0200

    fix: minor corrections

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,23 +3,26 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'airbnb-base',
-    'airbnb-typescript',
-  ],
-  parser: '@typescript-eslint/parser',
+  extends: ["airbnb-base", "airbnb-typescript"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: './tsconfig.json'
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
+  plugins: ["@typescript-eslint"],
   rules: {
+    "@typescript-eslint/quotes": [
+      "error",
+      "double",
+      {
+        allowTemplateLiterals: true,
+      },
+    ],
+    "linebreak-style": 0,
     "comma-dangle": "off",
     "@typescript-eslint/comma-dangle": [2],
-    'react/jsx-filename-extension': [0],
-    "@typescript-eslint/indent": [0]
+    "react/jsx-filename-extension": [0],
+    "@typescript-eslint/indent": [0],
   },
 };
