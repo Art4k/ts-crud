220e0cd43cfebf2156539adbbcd580a94adb2e7b
Artūras <arturas008@gmail.com>
2023-01-18 19:02:18 +0200

    feat(12-3): added table properties, constructor/methods

--- src/components/app.ts
+++ src/components/app.ts
@@ -1,8 +1,21 @@
+import CarsCollection from "../helpers/cars-collection";
+import stringifyProps from "../helpers/stingify-object";
+
+import Table from "./table";
+
+import cars from "../data/cars";
+import brands from "../data/brands";
+import models from "../data/models";
+
 class App {
   private htmlElement: HTMLElement;
 
+  private carsCollection: CarsCollection;
+
   constructor(selector: string) {
     const foundElement = document.querySelector<HTMLElement>(selector);
+
+    this.carsCollection = new CarsCollection({ cars, brands, models });
 
     if (foundElement === null) throw new Error(`Nerastas elementas su selektoriumi '${selector}'`);
 
@@ -10,9 +23,21 @@ class App {
   }
 
   initialize = (): void => {
-    const container = document.createElement('div');
-    container.className = 'container my-5';
-    container.innerHTML = 'Laukiu kol būsiu sukurtas';
+    const carTable = new Table({
+      title: "Visi automobiliai",
+      columns: {
+        id: "Id",
+        brand: "Markė",
+        model: "Modelis",
+        price: "Kaina",
+        year: "Metai",
+      },
+      rowsData: this.carsCollection.getAllCars.map(stringifyProps),
+    });
+
+    const container = document.createElement("div");
+    container.className = "container my-5";
+    container.appendChild(carTable.htmlElement);
 
     this.htmlElement.append(container);
   };
