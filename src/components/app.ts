d245292c4e912f525eca89ff9a56ab7c4bf6dbb1
Artūras <arturas008@gmail.com>
2023-01-19 21:06:27 +0200

    feat: added select field component

--- src/components/app.ts
+++ src/components/app.ts
@@ -2,7 +2,8 @@ import CarsCollection from "../helpers/cars-collection";
 import stringifyProps from "../helpers/stingify-object";
 
 import Table from "./table";
-
+import SelectField from "./selectfield";
+// Data imports
 import cars from "../data/cars";
 import brands from "../data/brands";
 import models from "../data/models";
@@ -12,6 +13,8 @@ class App {
 
   private carsCollection: CarsCollection;
 
+  private filterByBrand: SelectField;
+
   constructor(selector: string) {
     const foundElement = document.querySelector<HTMLElement>(selector);
 
@@ -20,6 +23,11 @@ class App {
     if (foundElement === null) throw new Error(`Nerastas elementas su selektoriumi '${selector}'`);
 
     this.htmlElement = foundElement;
+    this.filterByBrand = new SelectField({
+      label: "BrandName",
+      onChange: console.log("Filtruoja !"),
+      options: brands.map(({ id, title }) => ({ title, value: id })),
+    });
   }
 
   initialize = (): void => {
@@ -34,7 +42,6 @@ class App {
       },
       rowsData: this.carsCollection.getAllCars.map(stringifyProps),
     });
-
     const container = document.createElement("div");
     container.className = "container my-5";
     container.appendChild(carTable.htmlElement);
